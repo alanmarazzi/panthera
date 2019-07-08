db1996d80d31ec43a4fa52a36e8b903109b736ba
alanmarazzi <alan.marazzi@gmail.com>
2019-07-08 20:58:22 +0200

    Add examples and package alpha.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject panthera "0.1-alpha.5"
+(defproject panthera "0.1-alpha.6"
   :description "Data Frames in Clojure (with Pandas)"
   :url "https://github.com/alanmarazzi/panthera"
   :scm {:name "git" :url "https://github.com/alanmarazzi/panthera"}
