4df8a0a622eff5197f08523d0d7402f63a945791
Chris Nuernberger <chris@techascent.com>
2019-07-10 12:15:04 -0600

    Iteritems for iterating through pandas

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :scm {:name "git" :url "https://github.com/alanmarazzi/panthera"}
   :license {:name "EPL-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[cnuernber/libpython-clj "0.12"]
+  :dependencies [[cnuernber/libpython-clj "0.13"]
                  [org.clojure/core.memoize "0.7.2"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]]}})
