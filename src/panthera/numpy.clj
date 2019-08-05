bede38ffb61088d98f7e28961067fe7ddb354db3
alanmarazzi <alan.marazzi@gmail.com>
2019-08-05 11:08:30 +0200

    Add sklearn example

--- src/panthera/numpy.clj
+++ src/panthera/numpy.clj
@@ -1,8 +1,13 @@
 (ns panthera.numpy
   (:require
-    [libpython-clj.python :as py]))
+   [libpython-clj.python :as py]
+   [panthera.pandas.utils :as u]))
 
 (defonce numpy (py/import-module "numpy"))
+
+(defonce sk (py/import-module "sklearn"))
+
+(py/set-attr! sk "__SKLEARN_SETUP__" true)
 
 (defn py-get-in
   [py-module v]
@@ -10,6 +15,9 @@
     ((apply comp
             (reverse
              (map (fn [x] #(py/get-attr % x)) mods))) py-module)))
+
+(defn doc
+  [py-module ks])
 
 (defn module
   [py-module]
@@ -36,3 +44,47 @@
   (npy [:linalg :norm] {:args [[[1 2 3] [4 5 6]]]}) => 9.539..."
   ([k] ((module numpy) k))
   ([k args] (((module numpy) k) args)))
+
+(defn sklearn
+  ([k] ((module sk) k))
+  ([k args] (((module sk) k) args)))
+
+
+(comment
+  (def pokemon (pt/read-csv "resources/pokemon.csv"))
+
+  (def split (sklearn [:model_selection :train_test_split]
+                      {:args [(pt/subset-cols pokemon
+                                              "HP" "Attack"
+                                              "Defense" "Sp. Atk"
+                                              "Sp. Def" "Speed")
+                              (pt/subset-cols pokemon "Legendary")]
+                       :test_size 0.3}))
+
+  (defn train-test
+    [split k]
+    ((k {:x-train first
+         :x-test  second
+         :y-train #(% 2)
+         :y-test  last}) split))
+
+  (def logistic (sklearn [:linear_model :LogisticRegression]
+                         {:n_jobs -1 :solver "lbfgs"}))
+
+  (defn fit
+    [model x y]
+    (py/call-attr model "fit" x y))
+
+  (def model (fit model (train-test split :x-train) (train-test split :y-train)))
+
+  (defn predict
+    [model x]
+    (py/call-attr model "predict" x))
+
+  (predict model (train-test split :x-test))
+
+  (defn score
+    [model x y]
+    (py/call-attr model "score" x y))
+
+  (score model (train-test split :x-test) (train-test split :y-test)))
